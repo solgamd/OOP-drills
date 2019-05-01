9673196b07398b5b3a3ec46d59449ff2b24f33d2
solgamd <solgamd@gmail.com>
2019-05-01 13:06:27 +0200

    Final comments and touches to submit

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 let person1 = {
-    name: 'Don',
-
+    name: 'Bob',
     sayHello() {
         console.log(`Hello! My name is ${this.name}.`);
     }
 };
-
 let person2 = {
-    name: 'Michelle',
-
+    name: 'Linda',
     sayHello() {
         console.log(`Hello! My name is ${this.name}.`);
     }
 };
 let person3 = {
-    name: 'Kristen',
-
+    name: 'Tina',
     sayHello() {
         console.log(`Hello! My name is ${this.name}.`);
     }
 };
 let person4 = {
-    name: 'Hank',
-
+    name: 'Gene',
     sayHello() {
         console.log(`Hello! My name is ${this.name}.`);
     }
 };
 let person5 = {
-    name: 'Lucas',
-
+    name: 'Louise',
     sayHello() {
         console.log(`Hello! My name is ${this.name}.`);
     }
 };
-
 person1.sayHello();
 person2.sayHello();
 person3.sayHello();
 person4.sayHello();
 person5.sayHello();
+
+
+////// PSEUDO CLASSES -----------------------------------------------------------
 
 function Info(name, city, age) {
     this.name = name;
@@ -47,26 +43,61 @@ function Info(name, city, age) {
     this.age = age;
 }
 
-Info.prototype.whatsUp = function () {
+Info.prototype.whatsUp = function() {
     console.log('Hey! My name is ' + this.name + ', I am ' + this.age + ' years old and I live in ' + this.city + '.');
 }
 
-let p1 = new Info('Don', 'PR', '61');
+let p1 = new Info('Bob', 'Somewhere, NJ', '49');
 p1.whatsUp();
 
-let p2 = new Info('Michelle', 'PR', '59');
+let p2 = new Info('Linda', 'Somewhere, NJ', '47');
 p2.whatsUp();
 
-let p3 = new Info('Kristen', 'Okinawa', '33');
+let p3 = new Info('Tina', 'Somewhere, NJ', '14');
 p3.whatsUp();
 
-let p4 = new Info('Hank', 'Okinawa', '2');
+let p4 = new Info('Gene', 'Somewhere, NJ', '11');
 p4.whatsUp();
 
-let p5 = new Info('Lucas', 'Okinawa', '1');
+let p5 = new Info('Louise', 'Somewhere, NJ', '8');
 p5.whatsUp();
 
-////// ES6/ES2015 Syntax for OOP
+console.log(' '); // making space in the console 
+
+
+////// ES6/ES2015 Syntax 
+
+class moreInfo {
+    constructor(name, city, age) {
+        this.name = name;
+    this.city = city;
+    this.age = age;
+    }
+    aboutPerson() {
+        console.log('Hey! My name is ' + this.name + ', I am ' + this.age + ' years old and I live in ' + this.city + '.');
+    }
+}
+
+let belcher1 = new moreInfo('Bob', 'Somewhere, NJ', '49');
+belcher1.aboutPerson();
+
+let belcher2 = new moreInfo('Linda', 'Somewhere, NJ', '47');
+belcher2.aboutPerson();
+
+let belcher3 = new moreInfo('Tina', 'Somewhere, NJ', '14');
+belcher3.aboutPerson();
+
+let belcher4 = new moreInfo('Gene', 'Somewhere, NJ', '11');
+belcher4.aboutPerson();
+
+let belcher5 = new moreInfo('Louise', 'Somewhere, NJ', '8');
+belcher5.aboutPerson();
+
+console.log(' '); // making space in the console 
+
+
+////// INHERITANCE -----------------------------------------------------
+
 class Vehicle {
     constructor(manufac, wheels) {
         this.manufac = manufac;
@@ -81,21 +112,18 @@ let vehicle = new Vehicle('Cadillac', 4)
 console.log(vehicle);
 vehicle.aboutVehicle();
 
-console.log(' ');
-
 class Truck extends Vehicle {
     constructor(manufac, type, wheels, doors, bed) {
         super(manufac, wheels);
         this.doors = doors;
         this.type = type;
         this.bed = (type === 'truck' ? 'has' : 'does not have');
-    
     }
     aboutVehicle() {
-        console.log(`This is a ${this.manufac} ${this.type} with ${this.wheels} wheels, ${this.doors} doors, and it ${this.bed} a truckbed.`);
+        console.log(`This is a ${this.manufac} ${this.type} with ${this.wheels} wheels and ${this.doors} doors, and it ${this.bed} a truckbed.`);
     }
 }
-const newTruck = new Truck('Cadillac','truck', 4, 2);                 // this.bed condition => 'has'
+let newTruck = new Truck('Cadillac','truck', 4, 2);                 // this.bed condition => 'has'
 // const newTruck = new Truck('Cadillac','sedan', 4, 2);            // this.bed condition => 'does not have'
 console.log(newTruck);
 newTruck.aboutVehicle();
@@ -113,8 +141,8 @@ class Sedan extends Vehicle {
     }
 }
 let sedan = new Sedan('Cadillac','sedan', 4, 4, 'medium', 250);
-sedan.aboutVehicle();
 console.log(sedan);
+sedan.aboutVehicle();
 
 class Motorcycle extends Vehicle {
     constructor(manufac, wheels, doors, hdbars, steer) {
@@ -122,13 +150,13 @@ class Motorcycle extends Vehicle {
         this.type = 'motorcycle';
         this.doors = (doors === 0 ? `is a ${manufac} motorcycle` : `is not a ${manufac} motorcycle`);
         this.hdbars = (hdbars === true ? 'handlebars' : 'NO handlebars');
-        this.steer = (hdbars === true ? 'does NOT have a steering wheel' : 'has a steering wheel');
-        
+        this.steer = (hdbars === true ? 'does not have a steering wheel' : 'has a steering wheel');
     }  
     aboutVehicle() {
-        console.log(`This ${this.doors} so of course is has ${this.wheels} wheels, plus ${this.hdbars} (so it ${this.steer}.)`);
+        console.log(`This ${this.doors} so of course it has ${this.wheels} wheels and ${this.hdbars}, so it ${this.steer}.`);
     }
 }
 let moto = new Motorcycle('Royal Enfield', 2, 0, true, true);
-moto.aboutVehicle();
 console.log(moto);
+moto.aboutVehicle();
+
